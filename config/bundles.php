229bca23f6e9c1e3d145b75c65b434d578607202
Sinan Shaban <sinan.zikri@gmail.com>
2020-10-27 23:20:28 +0200

    Add Symfony Profiler

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true, 'fixtures' => true],
     Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
